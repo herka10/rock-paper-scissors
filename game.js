59e0b4e813626ca0ed70103833c68dc461a708b0
herka10 <kaonou.her10@gmail.com>
2022-09-12 19:37:29 -0500

    pseudo-code

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("Yo!")
+// define array for choices [r, p, s]
+// define var for computerChoice
+// define var for humanChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt humanChoice
+
+// randomly choose computerChoice
+
+// compare choices between humanChoice and computerChoice
+    // if humanChoice is p and computerChoice is r
+
+// display (alert) results (won, tied, lost round)
+
+// show stats (number of wins, losses, and ties)
+
+// play again? 
+    // restart game
+// else
+    // end game
+
